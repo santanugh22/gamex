51ee555992947bd772a996574ae88dcd21e8c99e
santanugh22 <126342367+Santanu-Tellis@users.noreply.github.com>
2024-10-05 22:25:25 +0530

    Home page is now done

--- App.js
+++ App.js
@@ -2,8 +2,32 @@ import "./global.css";
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import StackNavigator from "./src/navigator/StackNavigator";
-
+import * as SplashScreen from "expo-splash-screen";
+import { useFonts } from "expo-font";
+import { useEffect } from "react";
+SplashScreen.hideAsync();
 export default function App() {
+  const [loaded, error] = useFonts({
+    "Poppins-Black": require("./assets/fonts/Poppins-Black.ttf"),
+    "Poppins-Bold": require("./assets/fonts/Poppins-Bold.ttf"),
+    "Poppins-ExtraBold": require("./assets/fonts/Poppins-ExtraBold.ttf"),
+
+    "Poppins-Medium": require("./assets/fonts/Poppins-Medium.ttf"),
+    "Poppins-Regular": require("./assets/fonts/Poppins-Regular.ttf"),
+    "Poppins-SemiBold": require("./assets/fonts/Poppins-SemiBold.ttf"),
+    "Poppins-Thin": require("./assets/fonts/Poppins-Thin.ttf"),
+    "Poppins-ExtraLight": require("./assets/fonts/Poppins-ExtraLight.ttf"),
+  });
+
+  useEffect(() => {
+    if (loaded || error) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded, error]);
+
+  if (!loaded && !error) {
+    return null;
+  }
   return (
     <View style={styles.container}>
       <StackNavigator />
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,9 +1,19 @@
-import { StyleSheet, Text, View, SafeAreaView, Image,Dimensions } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  Image,
+  Dimensions,
+  TouchableHighlight,
+} from "react-native";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 import MaterialIcons from "@expo/vector-icons/MaterialIcons";
+import AntDesign from "@expo/vector-icons/AntDesign";
+import FontAwesome from "@expo/vector-icons/FontAwesome";
+import FontAwesome6 from "@expo/vector-icons/FontAwesome6";
 
-
-const {height:HEIGHT,width:WIDTH}=Dimensions.get("screen")
+const { height: HEIGHT, width: WIDTH } = Dimensions.get("screen");
 const Home = () => {
   const insets = useSafeAreaInsets();
   return (
@@ -28,18 +38,108 @@ const Home = () => {
         <View style={styles.header}>
           <MaterialIcons name="settings" size={34} color="white" />
         </View>
-        <View>
-          <Text className="text-4xl text-white">Never</Text>
+
+        {/* Text */}
+        <View className="self-center pt-3 gap-3">
+          <View>
+            <Text className="text-5xl text-green-500 font-[Poppins-Bold] tracking-[-1] text-center">
+              NEVER
+            </Text>
+            <View className="h-[2] w-[100] bg-green-500 -top-4 left-7" />
+          </View>
+          <View>
+            <Text className="text-5xl text-gray-100 font-[Poppins-Bold] tracking-[-1] text-center">
+              HAVE I
+            </Text>
+            <View className="h-[2] w-[80] bg-white -top-4 left-16" />
+          </View>
+          <View>
+            <Text className="text-5xl text-red-400 font-[Poppins-Bold] tracking-[-1] text-center">
+              EVER
+            </Text>
+            <View className="h-[2] w-[80] bg-red-400 -top-4 left-5" />
+          </View>
+        </View>
+
+        {/*  Buttons*/}
+        <View className="self-center justify-center items-center mt-16 gap-10">
+          <View>
+            <TouchableHighlight>
+              <View className="flex-row justify-between items-center gap-7 h-[60] w-[250] bg-green-500 rounded-2xl">
+                <View className=" justify-center items-center px-4">
+                  <Text>
+                    <AntDesign name="play" size={27} />
+                  </Text>
+                </View>
+                <View className="justify-center items-center px-14">
+                  <Text className="font-[Poppins-SemiBold] text-3xl mt-2">
+                    PLAY
+                  </Text>
+                </View>
+              </View>
+            </TouchableHighlight>
+          </View>
+
+          <View>
+            <TouchableHighlight>
+              <View className="flex-row justify-around items-center  h-[60] w-[250] bg-red-500 rounded-2xl">
+                <View className=" justify-center items-center">
+                  <Text>
+                    <FontAwesome name="group" size={24} />
+                  </Text>
+                </View>
+                <View className="justify-center items-center ">
+                  <Text className="font-[Poppins-SemiBold] text-3xl mt-2 text-center">
+                    MULTIPLAYER
+                  </Text>
+                </View>
+              </View>
+            </TouchableHighlight>
+          </View>
+
+          <View>
+            <TouchableHighlight>
+              <View className="flex-row justify-around items-center  h-[60] w-[250] bg-yellow-400 rounded-2xl">
+                <View className=" justify-center items-center">
+                  <Text>
+                    <FontAwesome6 name="gamepad" size={27} />
+                  </Text>
+                </View>
+                <View className="justify-center items-center ">
+                  <Text className="font-[Poppins-SemiBold] text-3xl mt-2">
+                    HOW TO PLAY
+                  </Text>
+                </View>
+              </View>
+            </TouchableHighlight>
+          </View>
+        </View>
+
+        {/* Footer */}
+        <View className="flex flex-row mt-36 justify-around">
+          <View className="justify-center items-center flex-row">
+            <View>
+              <Text className="text-white">
+                <MaterialIcons name="rocket" size={27} />
+              </Text>
+            </View>
+            <View className="border px-1 py-1  rounded-3xl border-white ">
+              <Text className="text-gray-50">FOLLOW US</Text>
+            </View>
+          </View>
+
+          <View className="flex-row justify-center items-center gap-1">
+            <View>
+              <Text className="text-white">
+                <FontAwesome name="codiepie" size={24}  />
+              </Text>
+            </View>
+            <View className="border px-1 py-1  rounded-3xl border-white ">
+              <Text className="text-gray-50">MORE GAMES</Text>
+            </View>
+          </View>
         </View>
       </View>
-
-      {/* Text */}
-      <View></View>
-
-      {/*  Buttons*/}
-
-      {/* Footer */}
-      <View></View>
     </SafeAreaView>
   );
 };
